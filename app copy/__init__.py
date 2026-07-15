829d1c926206320d2c122aa08ea616afb8c2ae90
DamnitDavid <devittodes@pm.me>
2026-07-15 00:32:53 +0000

    cleanup

--- app copy/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Proins - Quality Event Management System for Manufacturing SMBs"""
-
-__version__ = "0.1.0"
-__author__ = "Proins Team"
--- app copy/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SQLAlchemy models for Proins."""
-
-from app.models.user import User
-from app.models.event import Event
-
-__all__ = ["User", "Event"]
